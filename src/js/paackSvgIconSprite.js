86d12eb1f978d8fa98f52fad63506231d3fc475a
Jouderian <jouderianjr@gmail.com>
2020-08-27 11:12:16 +0200

    Remove useless constructor

--- src/js/paackSvgIconSprite.js
+++ src/js/paackSvgIconSprite.js
@@ -3,10 +3,6 @@ import { readFileSync } from 'fs';
 const svg = readFileSync('./dist/icon-sprite.svg', 'utf-8');
 
 class PaackSvgIconSprite extends HTMLElement {
-  constructor() {
-    super();
-  }
-
   static get observedAttributes() {
     return [];
   }
